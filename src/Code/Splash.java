b0f3e99c6d1e2c8ede8e51d95a28758ae87803c4
robicodes <blessedethio@gmail.com>
2014-12-11 09:38:02 +0300

    Change the icon on the splash screen

--- src/Code/Splash.java
+++ src/Code/Splash.java
@@ -11,18 +11,17 @@ public class Splash extends Canvas{
         main = m;
         setFullScreenMode(true);
     }
-
     protected void paint(Graphics g) {
         try {
             g.setColor(255, 255, 255);
             g.fillRect(0, 0, getWidth(), getHeight());
             int i=0;
-            while(i<6)
-            {
-                g.setColor(170 + (i*5), 170 + (i*5), 170 + (i*5));
-                g.drawRect(i, i, getWidth()-(i*2), getHeight()-(i*2));
-                i++;
-            }
+//            while(i<6)
+//            {
+//                g.setColor(170 + (i*5), 170 + (i*5), 170 + (i*5));
+//                g.drawRect(i, i, getWidth()-(i*2), getHeight()-(i*2));
+//                i++;
+//            }
             g.drawImage(Image.createImage("/image/splash.png"), getWidth() / 2, getHeight() / 5, Graphics.TOP | Graphics.HCENTER);
 
         } catch (IOException ex) {
