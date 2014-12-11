d69a3282738ec7a0e319af7737e6a95564e0b11c
robicodes <blessedethio@gmail.com>
2014-12-11 08:03:59 +0300

    add a new splash screen

---
+++ src/Code/Splash.java
@@ -0,0 +1,44 @@
+package code;
+
+
+import java.io.IOException;
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+
+
+public class Splash extends Canvas{
+
+    public Splash() {
+        setFullScreenMode(true);
+    }
+
+    protected void paint(Graphics g) {
+        try {
+            g.setColor(170, 170, 170);
+            g.fillRect(0, 0, getWidth(), getHeight());
+            int i=0;
+            while(i<6)
+            {
+                g.setColor(170 + (i*5), 170 + (i*5), 170 + (i*5));
+            g.drawRect(i, i, getWidth()-(i*2), getHeight()-(i*2));
+            i++;
+            }
+           
+            g.drawImage(Image.createImage("/img/splash.png"), getWidth() / 2, getHeight() / 5, Graphics.TOP | Graphics.HCENTER);
+
+        } catch (IOException ex) {
+        }
+    }
+ 
+    public void backToGodTools()
+    {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ex) {
+        }
+     Display.getDisplay(menu.m).setCurrent(menu.myContentCanvas);
+    }
+
+}
